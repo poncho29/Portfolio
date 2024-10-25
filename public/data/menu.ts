2bc8ac6afd9dce51811eb56254558d31497096e1
poncho29 <meneses0828@gmail.com>
2024-10-24 22:29:57 -0500

    fix: hidden route

--- public/data/menu.ts
+++ public/data/menu.ts
@@ -2,5 +2,5 @@ import { IMenu } from "@/interfaces";
 
 export const MENU_LINKS: IMenu[] = [
   { id: 1, label: 'Inicio', href: '/' },
-  { id: 2, label: 'Blog', href: '/blog' }
+  // { id: 2, label: 'Blog', href: '/blog' }
 ];
